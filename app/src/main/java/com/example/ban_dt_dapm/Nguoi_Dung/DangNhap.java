176e9a1462a195f6545342740bd2b110433f4e66
Tedokoji <tedokojikagami@gmail.com>
2023-10-13 20:27:51 +0700

    research Firebase

--- app/src/main/java/com/example/ban_dt_dapm/Nguoi_Dung/DangNhap.java
+++ app/src/main/java/com/example/ban_dt_dapm/Nguoi_Dung/DangNhap.java
@@ -7,10 +7,28 @@ import android.os.Bundle;
 import com.example.ban_dt_dapm.R;
 
 public class DangNhap extends AppCompatActivity {
-
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signInRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_nhap);
+        oneTapClient = Identity.getSignInClient(this);
+        signInRequest = BeginSignInRequest.builder()
+                .setPasswordRequestOptions(PasswordRequestOptions.builder()
+                        .setSupported(true)
+                        .build())
+                .setGoogleIdTokenRequestOptions(GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.default_web_client_id))
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(true)
+                        .build())
+                // Automatically sign in when exactly one credential is retrieved.
+                .setAutoSelectEnabled(true)
+                .build();
+
     }
+
 }
