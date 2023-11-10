baf112d37d8c8951c96217f2f8c19dd4d1054255
TinhsBen <trungtinhs154@gmail.com>
2023-11-10 14:25:57 +0700

    Chuc nang gio hang

--- app/src/main/java/com/example/ban_dt_dapm/Nguoi_Dung/GioHang.java
+++ app/src/main/java/com/example/ban_dt_dapm/Nguoi_Dung/GioHang.java
@@ -1,20 +1,77 @@
 package com.example.ban_dt_dapm.Nguoi_Dung;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 
 import com.example.ban_dt_dapm.R;
 import com.example.ban_dt_dapm.Xu_Ly_Giao_Dien.DrawerBaseActivity;
+import com.example.ban_dt_dapm.Xy_Ly_Danh_Sach.adapter_gio_hang;
+import com.example.ban_dt_dapm.Xy_Ly_Danh_Sach.get_set_san_pham;
 import com.example.ban_dt_dapm.databinding.ActivityGioHangBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class GioHang extends DrawerBaseActivity {
     ActivityGioHangBinding activityGioHangBinding;
+    RecyclerView rvListC;
+    ArrayList<get_set_san_pham> lstGetSetC;
+    adapter_gio_hang adapter_recyclerviewC;
+    SharedPreferences sharedPreferences_so_dien_thoai_nguoi_dung;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         activityGioHangBinding = ActivityGioHangBinding.inflate(getLayoutInflater());
         setContentView(activityGioHangBinding.getRoot());
         allocateActivityTitle("Giỏ hàng của bạn");
+        //
+        rvListC = findViewById(R.id.rvList3);
+        lstGetSetC = new ArrayList<>();
+        adapter_recyclerviewC = new adapter_gio_hang(lstGetSetC,this);
+        LinearLayoutManager linearLayoutManager =new LinearLayoutManager(GioHang.this,LinearLayoutManager.VERTICAL,false);
+        rvListC.setAdapter(adapter_recyclerviewC);
+        rvListC.setLayoutManager(linearLayoutManager);
+        //
+        AnhXa();
+    }
+    private void AnhXa() {
+        sharedPreferences_so_dien_thoai_nguoi_dung = getSharedPreferences("sdt_nguoi_dung", MODE_PRIVATE);
+        String sodienthoai = sharedPreferences_so_dien_thoai_nguoi_dung.getString("sdt_nguoi_dung", "");
+        DatabaseReference gioHangRef = FirebaseDatabase.getInstance().getReference().child("Nguoi_Dung")
+                .child(sodienthoai)
+                .child("GioHang");
+        gioHangRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                lstGetSetC.clear(); // Xóa danh sách hiện tại để làm mới dữ liệu
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String tenSP = snapshot.child("tenSP").getValue(String.class);
+                    String giaTien = snapshot.child("giaTien").getValue(String.class);
+                    String hinhAnh = snapshot.child("hinhAnh").getValue(String.class);
+                    String moTa = snapshot.child("moTa").getValue(String.class);
+                    String soLuong = snapshot.child("soLuong").getValue(String.class);
+                    get_set_san_pham sanPham = new get_set_san_pham(tenSP,moTa,giaTien,hinhAnh,soLuong,"");
+                    lstGetSetC.add(sanPham);
+                }
+                adapter_recyclerviewC.notifyDataSetChanged(); // Cập nhật RecyclerView khi có thay đổi dữ liệu
+            }
+
+            @Override
+            public void onCancelled(@androidx.annotation.NonNull DatabaseError error) {
+
+            }
+
+
+        });
     }
 }
---
+++ app/src/main/java/com/example/ban_dt_dapm/Xy_Ly_Danh_Sach/adapter_gio_hang.java
@@ -0,0 +1,167 @@
+package com.example.ban_dt_dapm.Xy_Ly_Danh_Sach;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ban_dt_dapm.Nguoi_Dung.DatHang;
+import com.example.ban_dt_dapm.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class adapter_gio_hang extends RecyclerView.Adapter<adapter_gio_hang.UserViewHolder> {
+    ArrayList<get_set_san_pham> lstGetSet;
+    Context context;
+    SharedPreferences sharedPreferences_so_dien_thoai_nguoi_dung;
+
+    public adapter_gio_hang(ArrayList<get_set_san_pham> lstGetSet, Context context) {
+        this.lstGetSet = lstGetSet;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public adapter_gio_hang.UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        //nap layout
+        View userView = inflater.inflate(R.layout.item_gio_hang,parent,false);
+        //
+        UserViewHolder viewHolder = new UserViewHolder(userView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull adapter_gio_hang.UserViewHolder holder, int position) {
+        get_set_san_pham item = lstGetSet.get(position);
+        holder.tvTen_spC.setText(item.getTenSP());
+        holder.tvGia_spC.setText(item.getGiaTien());
+        holder.tvSoluong_spC.setText(item.getSoLuong());
+        Picasso.get().load(item.getHinhAnh()).into(holder.ivHinh_anh_san_phamC);
+        holder.button_tangC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int soLuong = Integer.parseInt(holder.tvSoluong_spC.getText().toString());
+                soLuong++;
+                holder.tvSoluong_spC.setText(String.valueOf(soLuong));
+                item.setSoLuong(String.valueOf(soLuong));
+                // Cập nhật số lượng sản phẩm lên firebase
+                sharedPreferences_so_dien_thoai_nguoi_dung = context.getSharedPreferences("sdt_nguoi_dung",MODE_PRIVATE);
+                String sodienthoai = sharedPreferences_so_dien_thoai_nguoi_dung.getString("sdt_nguoi_dung","");
+                DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+                DatabaseReference gioHangRef = databaseReference.child("Nguoi_Dung")
+                        .child(sodienthoai)
+                        .child("GioHang")
+                        .child(item.getTenSP())
+                        .child("soLuong");
+                // Cập nhật số lượng sản phẩm trên Firebase
+                gioHangRef.setValue(String.valueOf(soLuong));
+            }
+        });
+        holder.button_giamC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int soLuong = Integer.parseInt(holder.tvSoluong_spC.getText().toString());
+                if (soLuong > 1) {
+                    soLuong--;
+                    holder.tvSoluong_spC.setText(String.valueOf(soLuong));
+                    item.setSoLuong(String.valueOf(soLuong));
+                    // Cập nhật số lượng sản phẩm lên firebase
+                    sharedPreferences_so_dien_thoai_nguoi_dung = context.getSharedPreferences("sdt_nguoi_dung",MODE_PRIVATE);
+                    String sodienthoai = sharedPreferences_so_dien_thoai_nguoi_dung.getString("sdt_nguoi_dung","");
+                    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+                    DatabaseReference gioHangRef = databaseReference.child("Nguoi_Dung")
+                            .child(sodienthoai)
+                            .child("GioHang")
+                            .child(item.getTenSP())
+                            .child("soLuong");
+                    // Cập nhật số lượng sản phẩm trên Firebase
+                    gioHangRef.setValue(String.valueOf(soLuong));
+                }
+            }
+        });
+        holder.iv_xoa_san_phamC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = holder.getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+                    get_set_san_pham item = lstGetSet.get(position);
+                    sharedPreferences_so_dien_thoai_nguoi_dung = context.getSharedPreferences("sdt_nguoi_dung",MODE_PRIVATE);
+                    String sodienthoai = sharedPreferences_so_dien_thoai_nguoi_dung.getString("sdt_nguoi_dung", "");
+                    lstGetSet.remove(position);
+                    notifyItemRemoved(position);
+                    notifyItemRangeChanged(position, lstGetSet.size());
+                    DatabaseReference gioHangRef = FirebaseDatabase.getInstance().getReference()
+                            .child("Nguoi_Dung")
+                            .child(sodienthoai)
+                            .child("GioHang").child(item.getTenSP());
+                    gioHangRef.removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            // Xóa thành công, cập nhật giao diện và hiển thị thông báo
+                            Toast.makeText(context, "Xóa sản phẩm thành công", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // Xử lý khi xóa thất bại
+                            Toast.makeText(context, "Xóa sản phẩm thất bại", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                }
+            }
+        });
+        holder.iv_dat_hangC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(view.getContext(), DatHang.class);
+                // Put the data you want to pass as extras
+                intent.putExtra("tenSP_dat_hang", item.getTenSP());
+                intent.putExtra("giaTien_dat_hang", item.getGiaTien());
+                intent.putExtra("hinhAnh_dat_hang",item.getHinhAnh());
+                intent.putExtra("soLuong_dat_hang", item.getSoLuong());
+                view.getContext().startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return lstGetSet.size();
+    }
+
+    public class UserViewHolder extends RecyclerView.ViewHolder {
+        ImageView ivHinh_anh_san_phamC,button_giamC,button_tangC;
+        TextView tvTen_spC,tvGia_spC,tvSoluong_spC;
+        ImageView iv_xoa_san_phamC,iv_dat_hangC;
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivHinh_anh_san_phamC = itemView.findViewById(R.id.ivHinh_anh_san_pham);
+            tvTen_spC = itemView.findViewById(R.id.tvTen_sp);
+            tvGia_spC = itemView.findViewById(R.id.tvGia_sp);
+            tvSoluong_spC = itemView.findViewById(R.id.tvSoluong_sp);
+            iv_xoa_san_phamC = itemView.findViewById(R.id.iv_xoa_san_pham);
+            iv_dat_hangC = itemView.findViewById(R.id.iv_dat_hang);
+            button_giamC = itemView.findViewById(R.id.button_giam);
+            button_tangC = itemView.findViewById(R.id.button_tang);
+        }
+    }
+}
